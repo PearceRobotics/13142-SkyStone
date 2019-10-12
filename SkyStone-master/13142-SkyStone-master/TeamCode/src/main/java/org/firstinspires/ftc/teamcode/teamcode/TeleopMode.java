cf3889f2381303e734851a83003b8b9a347e73ed
Eliza Barnett <elizacatbarnett@gmail.com>
2019-10-11 21:33:47 -0500

    Added position code for lift. Not tested yet. Code is commented out because we first need to figure out the encoder values for each positions.

--- SkyStone-master/13142-SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/TeleopMode.java
+++ SkyStone-master/13142-SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/TeleopMode.java
@@ -15,6 +15,7 @@ public class TeleopMode extends LinearOpMode
     private DcMotor motorLeft;
     private DcMotor motorRight;
     private DcMotor fourBar;
+    private DcMotor fourBar2;
     private Servo leftServo;
     private Servo rightServo;
     private Servo intakeLeft;
@@ -23,8 +24,16 @@ public class TeleopMode extends LinearOpMode
     //Variables
     static final double maxPosition = 0.0;
     static final double minPosition = 1.0;
-    static final double lifterUp = 0.0;
-    static final double lifterDown = 1.0;
+    static final double IntakeUp = 0.0;
+    static final double IntakeDown = 1.0;
+    static final int PositionA = 0;
+    static final int PositionB = 100;
+    static final int PositionC = 200;
+    static final int PositionD = 300;
+    static boolean positionA;
+    static boolean positionB;
+    static boolean positionC;
+    static boolean positionD;
 
 
     @Override
@@ -34,13 +43,25 @@ public class TeleopMode extends LinearOpMode
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
         fourBar = hardwareMap.dcMotor.get("lifter");
+        fourBar2 = hardwareMap.dcMotor.get("lifter2");
         intakeLeft = hardwareMap.get(Servo.class, "intakeLeft");
         intakeRight = hardwareMap.get(Servo.class, "intakeRight");
         leftServo = hardwareMap.get(Servo.class, "leftServo");
         rightServo = hardwareMap.get(Servo.class, "rightServo");
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        fourBar2.setDirection(DcMotor.Direction.REVERSE);
         intakeLeft.setDirection(Servo.Direction.REVERSE);
+
+        positionA = true;
+        positionB = false;
+        positionC = false;
+        positionD = false;
+
+        //fourBar.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       // fourBar2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+       // fourBar.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      //  fourBar2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         waitForStart();
 
@@ -62,29 +83,55 @@ public class TeleopMode extends LinearOpMode
             }
 
             //Moves intake up
-            if(gamepad2.y)
+            if(gamepad2.left_bumper)
             {
-                intakeMechanism(lifterUp);
+                intakeMechanism(IntakeUp);
             }
             //Moves intake down
-            if(gamepad2.a)
+            if(gamepad2.right_bumper)
             {
-                intakeMechanism(lifterDown);
+                intakeMechanism(IntakeDown);
             }
+
+           /* if(gamepad2.a)
+            {
+                positionA();
+            }
+            if(gamepad2.b)
+            {
+                positionB();
+            }
+            if(gamepad2.x)
+            {
+                positionC();
+            }
+            if(gamepad2.y)
+            {
+                positionD();
+            }*/
             //raises and lowers four bar linkage
             if(gamepad2.left_trigger> .01)
             {
-                raiseFourBar(-gamepad2.left_trigger);
+                raiseFourBar(-gamepad2.left_trigger*3/4);
+               telemetry.addData("current position",fourBar.getCurrentPosition());
+               telemetry.update();
             }
             if(gamepad2.right_trigger> 0.1) {
-                raiseFourBar(gamepad2.right_trigger);
+                raiseFourBar(gamepad2.right_trigger*3/4);
+                telemetry.addData("current position",fourBar.getCurrentPosition());
+                telemetry.update();
             }
             else
             {
-                fourBar.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                brakeFourBar();
             }
 
         }
+    }
+    public void brakeFourBar()
+    {
+        fourBar.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fourBar2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
     public void platformMoverPosition(double position)
     {
@@ -96,9 +143,97 @@ public class TeleopMode extends LinearOpMode
         intakeLeft.setPosition(position);
         intakeRight.setPosition(position);
     }
-    public void raiseFourBar(double intakeSpeed)
+    public void raiseFourBar(double liftSpeed)
     {
-        fourBar.setPower(intakeSpeed);
+        fourBar.setPower(liftSpeed);
+        fourBar2.setPower(liftSpeed);
     }
+  /*  public void positionA()
+    {
+        if(positionB = true)
+        {
+            positionCode(-PositionB);
+            positionB = false;
+        }
+        if(positionC = true);
+        {
+            positionCode(-PositionC);
+            positionC = false;
+        }
+        if(positionD = true)
+        {
+            positionCode(-PositionD);
+            positionD = false;
+        }
+        positionA = true;
+    }
+    public void positionB()
+    {
+        if(positionA = true)
+        {
+            positionCode((PositionB - PositionA));
+            positionA = false;
+        }
+        if(positionC = true)
+        {
+            positionCode((PositionB - PositionC));
+            positionC = false;
+        }
+        if(positionD = true)
+        {
+            positionCode((PositionB - PositionD));
+            positionD = false;
+        }
+        positionB = true;
+    }
+    public void positionC()
+    {
+        if(positionA = true)
+        {
+            positionCode((PositionC - PositionA));
+            positionA = false;
+        }
+        if(positionB = true)
+        {
+            positionCode((PositionC -PositionB));
+            positionB = false;
+        }
+        if(positionD = true)
+        {
+            positionCode((PositionC-PositionD));
+            positionD = false;
+        }
+        positionC = true;
+    }
+    public void positionD()
+    {
+        if(positionA = true)
+        {
+            positionCode((PositionD - PositionA));
+            positionA = false;
+        }
+        if(positionB = true)
+        {
+            positionCode((PositionD - PositionB));
+            positionB = false;
+        }
+        if(positionC = true)
+        {
+            positionCode((PositionD - PositionC));
+            positionC = false;
+        }
+        positionD = true;
+    }
+
+    public void positionCode( int TargetPosition)
+    {
+        fourBar.setTargetPosition(TargetPosition);
+        fourBar2.setTargetPosition(TargetPosition);
+        while(fourBar.isBusy()|| fourBar2.isBusy() && opModeIsActive()) {
+            //Loop body can be empty
+        }
+        fourBar.setPower(0);
+    }*/
+
 
 }
